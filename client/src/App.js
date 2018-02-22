ce12ec0bf3db198709b60a73fa00aeb1c6c9c7c1
Manmeet Tarun <meetutarun16@gmail.com>
2018-02-23 05:07:04 +0530

    Assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Landing from './components/home';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import {fetchPost} from '../action/fetch_post';
 import _ from 'lodash';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import AI from './assets/AI.png';
 const api="PMQ8-5I2N-T6MN-S1ZI";
 
 
 class Landing extends Component{
     
     componentDidMount(){
-        this.props.fetchPost()
+        this.props.fetchPost();
     }
 
     renderPost(){
         return _.map(this.props.posts,post => {
             return (
-                <li className="list-group-items" key={post.id}>
-                {post.title} <br/>
-                {post.content}
+                <li className="list-group-item" key={post.id}>
+                <div className="col-lg-6 imageContainer">
+                <img src={AI} alt="AI" />
+                </div><br/>
+                <h3>Title:  {post.title}</h3>  <br/>
+                <p>Content: {post.content}</p>
                 </li>
             )
         });
@@ -26,7 +31,11 @@ class Landing extends Component{
         return (
             <div style={{textAlign: 'center'}}>
             <h1>Blog</h1>
+            <div class="container">
+            <ul className="list-group">
             {this.renderPost()}
+            </ul>
+            </div>
             </div>
         );
 };
